8d082da31b4358047a938da6897574b68205254a
Fitch <fitch@mystiko.network>
2022-01-16 03:21:25 +0800

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,95 @@
 const path = require('path');
 const webpack = require('webpack');
-module.exports = {
-  output: {
-    path: path.resolve(__dirname, 'build/js'),
-    library: 'mystiko',
-    libraryTarget: 'umd',
-    libraryExport: 'default',
-    filename: 'mystiko.js',
-  },
-  mode: 'development',
-  target: 'web',
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-transform-runtime'],
+const TerserPlugin = require('terser-webpack-plugin');
+module.exports = [
+  {
+    output: {
+      path: path.resolve(__dirname, 'build/js'),
+      library: 'mystiko',
+      libraryTarget: 'umd',
+      libraryExport: 'default',
+      filename: 'mystiko.js',
+    },
+    mode: 'production',
+    target: 'web',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
           },
         },
+      ],
+    },
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        path: require.resolve('path-browserify'),
+        fs: false,
       },
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['safe-buffer', 'Buffer'],
+        process: 'process',
+      }),
     ],
-  },
-  resolve: {
-    fallback: {
-      crypto: require.resolve('crypto-browserify'),
-      stream: require.resolve('stream-browserify'),
-      os: require.resolve('os-browserify/browser'),
-      path: require.resolve('path-browserify'),
-      fs: false,
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+        }),
+      ],
     },
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-      Buffer: ['safe-buffer', 'Buffer'],
-      process: 'process',
-    }),
-  ],
-};
+  {
+    output: {
+      path: path.resolve(__dirname, 'build/js'),
+      library: 'mystiko',
+      libraryTarget: 'commonjs',
+      libraryExport: 'default',
+      filename: 'mystiko.cjs',
+    },
+    resolve: {
+      extensions: ['.node', '...'],
+    },
+    mode: 'production',
+    target: 'node',
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        },
+        {
+          test: /\.node$/,
+          loader: 'node-loader',
+        },
+      ],
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [
+        new TerserPlugin({
+          extractComments: false,
+        }),
+      ],
+    },
+  },
+];
