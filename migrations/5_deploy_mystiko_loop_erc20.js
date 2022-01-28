6aa2acf3133928a6f6fd983ab718ddeef46d7e37
ericlurker <96100006+ericlurker@users.noreply.github.com>
2022-01-28 21:02:16 +0800

    support mUSD (#29)

--- migrations/5_deploy_mystiko_loop_erc20.js
+++ migrations/5_deploy_mystiko_loop_erc20.js
@@ -5,16 +5,27 @@ const Verifier = artifacts.require('Verifier');
 const Hasher = artifacts.require('Hasher');
 const TestToken = artifacts.require('TestToken');
 
-module.exports = function (deployer) {
+module.exports = function (deployer, network) {
   return deployer.then(async () => {
-    const { MERKLE_TREE_HEIGHT } = process.env;
+    const { MERKLE_TREE_HEIGHT, ROPSTEN_ERC20_ADDRESS, BSC_TESTNET_ERC20_ADDRESS } = process.env;
+
     const verifier = await Verifier.deployed();
     const hasher = await Hasher.deployed();
-    const token = await TestToken.deployed();
+    const testToken = await TestToken.deployed();
+
+    var tokneAddress;
+    if (network === 'bsctestnet') {
+      tokneAddress = BSC_TESTNET_ERC20_ADDRESS;
+    } else if (network === 'ropsten') {
+      tokneAddress = ROPSTEN_ERC20_ADDRESS;
+    } else {
+      tokneAddress = testToken.address;
+    }
+
     await deployer.deploy(
       MystikoWithLoopERC20,
       verifier.address,
-      token.address,
+      tokneAddress,
       hasher.address,
       MERKLE_TREE_HEIGHT,
     );
