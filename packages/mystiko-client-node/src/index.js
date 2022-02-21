70f0782f800a7a87544c7fdfdfce0765484c903d
Fitch <fitch@mystiko.network>
2022-02-21 20:01:18 +0800

    add more test coverage report

--- packages/mystiko-client-node/src/index.js
+++ packages/mystiko-client-node/src/index.js
@@ -7,21 +7,6 @@ import { MystikoConfig } from '@mystiko/client/src/config';
 import DefaultTestnetConfigJson from '@mystiko/contracts/config/default/testnet.json';
 import DefaultMainnetConfigJson from '@mystiko/contracts/config/default/mainnet.json';
 
-const defaultTestnetCircuitsConfig = [
-  {
-    name: 'circom-1.0',
-    wasmFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.wasm.gz',
-    zkeyFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.zkey.gz',
-    vkeyFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.vkey.json.gz',
-  },
-];
-
-const DefaultTestnetConfig = new MystikoConfig({
-  ...DefaultTestnetConfigJson,
-  circuits: defaultTestnetCircuitsConfig,
-});
-const DefaultMainnetConfig = new MystikoConfig(DefaultMainnetConfigJson);
-
 const colors = {
   TRACE: chalk.magentaBright,
   DEBUG: chalk.cyanBright,
@@ -35,17 +20,33 @@ const loggingOptions = {
     return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)} ${chalk.green(`${name}:`)}`;
   },
 };
+
+const defaultTestnetCircuitsConfig = [
+  {
+    name: 'circom-1.0',
+    wasmFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.wasm.gz',
+    zkeyFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.zkey.gz',
+    vkeyFile: 'node_modules/@mystiko/circuits/dist/circom/dev/Withdraw.vkey.json.gz',
+  },
+];
+
+export const DefaultTestnetConfig = new MystikoConfig({
+  ...DefaultTestnetConfigJson,
+  circuits: defaultTestnetCircuitsConfig,
+});
+export const DefaultMainnetConfig = new MystikoConfig(DefaultMainnetConfigJson);
+
 const initialize = mystiko.initialize;
 mystiko.initialize = (options = {}) => {
+  if (options.isTestnet === undefined || options.isTestnet === null) {
+    options.isTestnet = true;
+  }
+  if (!options.conf) {
+    options.conf = options.isTestnet ? DefaultTestnetConfig : DefaultMainnetConfig;
+  }
   if (!options.dbFile) {
     if (!fs.existsSync('db')) {
       fs.mkdirSync('db');
-    }
-    if (options.isTestnet === undefined || options.isTestnet === null) {
-      options.isTestnet = true;
-    }
-    if (!options.conf) {
-      options.conf = options.isTestnet ? DefaultTestnetConfig : DefaultMainnetConfig;
     }
     options.dbFile = options.isTestnet ? 'db/mystiko_testnet.db' : 'db/mystiko.db';
   }
