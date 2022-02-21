519107d58b24098dcad5c713cd27f639fe5d211a
Fitch Li <fitch@mystiko.network>
2022-02-21 20:15:05 +0800

    add more test coverage report (#71)

--- packages/mystiko-client-browser/tests/index.test.js
+++ packages/mystiko-client-browser/tests/index.test.js
@@ -1,5 +1,17 @@
 import '../src/index.js';
+import { MystikoConfig } from '@mystiko/client/src/config';
+import DefaultTestnetConfigJson from '@mystiko/contracts/config/default/testnet.json';
+import DefaultMainnetConfigJson from '@mystiko/contracts/config/default/mainnet.json';
 
-test('test window is set', () => {
+const DefaultTestnetConfig = new MystikoConfig(DefaultTestnetConfigJson);
+const DefaultMainnetConfig = new MystikoConfig(DefaultMainnetConfigJson);
+
+test('test window is set', async () => {
   expect(window.mystiko).not.toBe(undefined);
+  await window.mystiko.initialize({ dbAdapter: undefined });
+  expect(window.mystiko.config).toStrictEqual(DefaultTestnetConfig);
+  await window.mystiko.initialize({ isTestnet: true, dbAdapter: undefined });
+  expect(window.mystiko.config).toStrictEqual(DefaultTestnetConfig);
+  await window.mystiko.initialize({ isTestnet: false, dbAdapter: undefined });
+  expect(window.mystiko.config).toStrictEqual(DefaultMainnetConfig);
 });
