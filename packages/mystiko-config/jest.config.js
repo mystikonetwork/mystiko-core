03c3f418f9c59f3d385b03c329f414c4d9fa8c18
Fitch <fitch@mystiko.network>
2022-02-24 21:17:07 +0800

    add jest coverage threshold

--- packages/mystiko-config/jest.config.js
+++ packages/mystiko-config/jest.config.js
@@ -4,5 +4,13 @@ module.exports = {
   testEnvironment: 'node',
   collectCoverage: true,
   coverageDirectory: 'coverage',
+  coverageThreshold: {
+    global: {
+      lines: 95,
+      branches: 90,
+      statements: 95,
+      functions: 95,
+    },
+  },
   testPathIgnorePatterns: ['/node_modules/', '/build/tests'],
 };
