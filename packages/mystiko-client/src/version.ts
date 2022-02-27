ebbaf894f1f810ec0c03e356cd9ecc3fa31acf1d
Fitch <fitch@mystiko.network>
2022-02-27 20:00:35 +0800

    change to 0.0.3

--- packages/mystiko-client/src/version.ts
+++ packages/mystiko-client/src/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = '0.0.2';
+export const VERSION = '0.0.3';
 export const VERSION_REGEX = /^\d+\.\d+\.\d+$/;
 export function isValidVersion(version: string | String): boolean {
   return !!version.match(VERSION_REGEX);
