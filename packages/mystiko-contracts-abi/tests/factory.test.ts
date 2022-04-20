188a6bc1995bd35d277e04d91b84df0921f00e72
Fitch <fitch@mystiko.network>
2022-04-19 19:29:06 -0700

    rename test variables

--- packages/mystiko-contracts-abi/tests/factory.test.ts
+++ packages/mystiko-contracts-abi/tests/factory.test.ts
@@ -16,63 +16,63 @@ beforeEach(() => {
 });
 
 test('test connect ERC20', () => {
-  const erc20 = MystikoContractFactory.connect<ERC20>(
+  const contract = MystikoContractFactory.connect<ERC20>(
     'ERC20',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test connect CommitmentPool', () => {
-  const erc20 = MystikoContractFactory.connect<CommitmentPool>(
+  const contract = MystikoContractFactory.connect<CommitmentPool>(
     'CommitmentPool',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test connect MystikoV2Loop', () => {
-  let erc20 = MystikoContractFactory.connect<MystikoV2Loop>(
+  let contract = MystikoContractFactory.connect<MystikoV2Loop>(
     'MystikoV2Loop',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
-  erc20 = MystikoContractFactory.connect<MystikoV2Loop>(
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  contract = MystikoContractFactory.connect<MystikoV2Loop>(
     'MystikoV2WithLoopERC20',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test connect MystikoV2Bridge', () => {
-  const erc20 = MystikoContractFactory.connect<MystikoV2Bridge>(
+  const contract = MystikoContractFactory.connect<MystikoV2Bridge>(
     'MystikoV2Bridge',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test connect MystikoV2WithCeler', () => {
-  const erc20 = MystikoContractFactory.connect<MystikoV2WithCeler>(
+  const contract = MystikoContractFactory.connect<MystikoV2WithCeler>(
     'MystikoV2WithCelerERC20',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test connect MystikoV2WithTBridge', () => {
-  const erc20 = MystikoContractFactory.connect<MystikoV2WithTBridge>(
+  const contract = MystikoContractFactory.connect<MystikoV2WithTBridge>(
     'MystikoV2WithTBridgeERC20',
     '0x5c7c88e07e3899fff3cc0effe23494591dfe87b6',
     provider,
   );
-  expect(erc20.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
+  expect(contract.address).toBe('0x5c7c88e07e3899fff3cc0effe23494591dfe87b6');
 });
 
 test('test invalid contractName', () => {
