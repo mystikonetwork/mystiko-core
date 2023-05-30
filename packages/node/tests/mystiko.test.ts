47a26fe650e3c6e9b17dd3ba30ec02e754cf6316
X <96100679+moomarco@users.noreply.github.com>
2023-05-30 18:18:36 +0800

    update gas relayer client and config version (#187)
    * update gas relayer client and config version
    
    * new tag 0.5.6
    
    * mock remote relayer config
    
    * lint

--- packages/node/tests/mystiko.test.ts
+++ packages/node/tests/mystiko.test.ts
@@ -1,6 +1,12 @@
+import nock from 'nock';
 import mystiko from '../src';
 
+const CONFIG_BASE_URL = 'https://static.mystiko.network/config';
+const RELAYER_CONFIG_BASE_URL = 'https://static.mystiko.network/relayer_config';
+
 test('test initialize', async () => {
+  nock(CONFIG_BASE_URL).get('/production/testnet/latest.json').reply(200, { version: '1.0.0' });
+  nock(RELAYER_CONFIG_BASE_URL).get('/production/testnet/latest.json').reply(200, { version: '1.0.0' });
   await mystiko.initialize({ dbInMemory: true });
   await mystiko.db?.destroy();
 });
