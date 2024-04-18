5065dc40718c96f58ffbefae07e9a4a1aa949c81
Fitch Li <fitch@mystiko.network>
2024-04-18 17:35:15 +0800

    add import and sync to assets handler

--- packages/core/src/handler/impl/v2/asset.ts
+++ packages/core/src/handler/impl/v2/asset.ts
@@ -1,14 +1,24 @@
 import { BridgeType, PoolContractConfig } from '@mystikonetwork/config';
+import { CommitmentPool } from '@mystikonetwork/contracts-abi';
 import { Chain, Commitment, CommitmentStatus } from '@mystikonetwork/database';
-import { fromDecimals, toBN } from '@mystikonetwork/utils';
-import { MystikoHandler } from '../../handler';
+import { fromDecimals, promiseWithTimeout, toBN } from '@mystikonetwork/utils';
+import { ethers } from 'ethers';
 import {
   AssetBalance,
   AssetBalanceOptions,
+  AssetChainImportOptions,
   AssetHandler,
+  AssetImportOptions,
   AssetMultipleBalanceOptions,
+  AssetSyncOptions,
+  CommitmentIncludedEvent,
+  CommitmentQueuedEvent,
+  EventType,
   MystikoContextInterface,
 } from '../../../interface';
+import { MystikoHandler } from '../../handler';
+
+export const DEFAULT_PROVIDER_QUERY_TIMEOUT_MS = 60000;
 
 export class AssetHandlerV2 extends MystikoHandler implements AssetHandler {
   constructor(context: MystikoContextInterface) {
@@ -124,6 +134,38 @@ export class AssetHandlerV2 extends MystikoHandler implements AssetHandler {
     return Promise.resolve([]);
   }
 
+  public async import(options: AssetImportOptions): Promise<Commitment[]> {
+    const chains = Array.isArray(options.chain) ? options.chain : [options.chain];
+    const commitments = await this.importFromProviders(options, chains);
+    if (commitments.length > 0) {
+      const updatedCommitments = await this.context.executors.getCommitmentExecutor().decrypt({
+        commitments,
+        walletPassword: options.walletPassword,
+      });
+      return this.checkCommitmentsSpent(updatedCommitments, options.providerTimeoutMs);
+    }
+    return [];
+  }
+
+  public async sync(options: AssetSyncOptions): Promise<Map<string, Map<string, AssetBalance>>> {
+    const commitments = await this.getCommitments([CommitmentStatus.QUEUED, CommitmentStatus.INCLUDED]);
+    await Promise.all(commitments.map((commitment) => this.syncCommitmentStatus(commitment, options)));
+    const accounts = await this.context.accounts.find();
+    const accountsBalances = await Promise.all(
+      accounts.map((account) =>
+        this.balances({ shieldedAddress: account.shieldedAddress }).then((balances) => ({
+          shieldedAddress: account.shieldedAddress,
+          balances,
+        })),
+      ),
+    );
+    const balances = new Map<string, Map<string, AssetBalance>>();
+    accountsBalances.forEach((accountBalances) => {
+      balances.set(accountBalances.shieldedAddress, accountBalances.balances);
+    });
+    return balances;
+  }
+
   private defaultShieldedAddresses(): Promise<string[]> {
     return this.context.accounts
       .find()
@@ -199,5 +241,224 @@ export class AssetHandlerV2 extends MystikoHandler implements AssetHandler {
       .reduce((c1, c2) => c1.add(c2), toBN(0));
     const decimal = commitments.length > 0 ? commitments[0].assetDecimals : undefined;
     return fromDecimals(total, decimal);
+  }
+
+  private async importFromProviders(
+    options: AssetImportOptions,
+    chainOptions: AssetChainImportOptions[],
+  ): Promise<Commitment[]> {
+    const tasks = await Promise.all(chainOptions.map((c) => this.importChainFromProviders(options, c)));
+    return tasks.flat();
+  }
+
+  private async importChainFromProviders(
+    options: AssetImportOptions,
+    chainOptions: AssetChainImportOptions,
+  ): Promise<Commitment[]> {
+    const provider = await this.context.providers.getProvider(chainOptions.chainId);
+    if (provider) {
+      const txHash = Array.isArray(chainOptions.txHash) ? chainOptions.txHash : [chainOptions.txHash];
+      const txReceiptTasks = txHash.map(async (hash) => {
+        const txReceipt = await promiseWithTimeout(
+          provider.getTransactionReceipt(hash),
+          options.providerTimeoutMs || DEFAULT_PROVIDER_QUERY_TIMEOUT_MS,
+        );
+        if (txReceipt && txReceipt.blockNumber) {
+          return this.importTxReceiptFromProvider(options, chainOptions, txReceipt, provider);
+        }
+        return Promise.resolve([]);
+      });
+      const commitments = await Promise.all(txReceiptTasks);
+      return commitments.flat();
+    }
+    return Promise.resolve([]);
+  }
+
+  private async importTxReceiptFromProvider(
+    options: AssetImportOptions,
+    chainOptions: AssetChainImportOptions,
+    txReceipt: ethers.providers.TransactionReceipt,
+    provider: ethers.providers.Provider,
+  ): Promise<Commitment[]> {
+    const contractAddress = txReceipt.to;
+    let contractConfig: PoolContractConfig | undefined;
+    let contract: CommitmentPool | undefined;
+    const depositContractConfig = this.config.getDepositContractConfigByAddress(
+      chainOptions.chainId,
+      contractAddress,
+    );
+    if (depositContractConfig && depositContractConfig.bridgeType === BridgeType.LOOP) {
+      contract = this.context.contractConnector.connect<CommitmentPool>(
+        'CommitmentPool',
+        depositContractConfig.poolAddress,
+        provider,
+      );
+      contractConfig = depositContractConfig.poolContract;
+    } else {
+      const poolContractConfig = this.config.getPoolContractConfigByAddress(
+        chainOptions.chainId,
+        contractAddress,
+      );
+      if (poolContractConfig) {
+        contract = this.context.contractConnector.connect<CommitmentPool>(
+          'CommitmentPool',
+          contractAddress,
+          provider,
+        );
+        contractConfig = poolContractConfig;
+      }
+    }
+    if (contract && contractConfig) {
+      const includedCount = (
+        await promiseWithTimeout(
+          contract.getCommitmentIncludedCount(),
+          options.providerTimeoutMs || DEFAULT_PROVIDER_QUERY_TIMEOUT_MS,
+        )
+      ).toNumber();
+      const queuedTopic = contract.interface.getEventTopic('CommitmentQueued');
+      const queuedEvents: CommitmentQueuedEvent[] = [];
+      const includedEvents: CommitmentIncludedEvent[] = [];
+      for (let i = 0; i < txReceipt.logs.length; i += 1) {
+        const log = txReceipt.logs[i];
+        if (log.topics.length > 0 && log.topics[0] === queuedTopic) {
+          const queuedEvent = contract?.interface.parseLog(log);
+          if (queuedEvent.args.leafIndex.toNumber() < includedCount) {
+            includedEvents.push({
+              eventType: EventType.COMMITMENT_INCLUDED,
+              contractAddress: contractConfig.address,
+              transactionHash: txReceipt.transactionHash,
+              commitmentHash: queuedEvent.args.commitment,
+              leafIndex: queuedEvent.args.leafIndex,
+              rollupFee: queuedEvent.args.rollupFee,
+              encryptedNote: queuedEvent.args.encryptedNote,
+              queuedTransactionHash: txReceipt.transactionHash,
+            });
+          } else {
+            queuedEvents.push({
+              eventType: EventType.COMMITMENT_QUEUED,
+              contractAddress: contractConfig.address,
+              transactionHash: txReceipt.transactionHash,
+              commitmentHash: queuedEvent.args.commitment,
+              leafIndex: queuedEvent.args.leafIndex,
+              rollupFee: queuedEvent.args.rollupFee,
+              encryptedNote: queuedEvent.args.encryptedNote,
+            });
+          }
+        }
+      }
+      return this.context.executors.getEventExecutor().import([...queuedEvents, ...includedEvents], {
+        chainId: chainOptions.chainId,
+        walletPassword: options.walletPassword,
+      });
+    }
+    return Promise.resolve([]);
+  }
+
+  private async checkCommitmentsSpent(
+    commitments: Commitment[],
+    providerTimeoutMs?: number,
+  ): Promise<Commitment[]> {
+    const updatedCommitments = await Promise.all(
+      commitments.map((commitment) => this.checkCommitmentSpent(commitment, providerTimeoutMs)),
+    );
+    return updatedCommitments.filter((c) => c.shieldedAddress && c.status !== CommitmentStatus.SPENT);
+  }
+
+  private async checkCommitmentSpent(
+    commitment: Commitment,
+    providerTimeoutMs?: number,
+  ): Promise<Commitment> {
+    const { serialNumber } = commitment;
+    if (serialNumber) {
+      const isSpent = await this.isKnowNullifierFromProvider(
+        commitment.chainId,
+        commitment.contractAddress,
+        serialNumber,
+        providerTimeoutMs,
+      );
+      if (isSpent) {
+        return commitment.atomicUpdate((commitmentData) => {
+          commitmentData.status = CommitmentStatus.SPENT;
+          commitmentData.updatedAt = MystikoHandler.now();
+          return commitmentData;
+        });
+      }
+    }
+    return commitment;
+  }
+
+  private async isKnowNullifierFromProvider(
+    chainId: number,
+    contractAddress: string,
+    nullifier: string,
+    providerTimeoutMs?: number,
+  ): Promise<boolean> {
+    const provider = await this.context.providers.getProvider(chainId);
+    if (provider) {
+      const contract = this.context.contractConnector.connect<CommitmentPool>(
+        'CommitmentPool',
+        contractAddress,
+        provider,
+      );
+      return promiseWithTimeout(
+        contract.isSpentSerialNumber(nullifier),
+        providerTimeoutMs || DEFAULT_PROVIDER_QUERY_TIMEOUT_MS,
+      );
+    }
+    return false;
+  }
+
+  private async syncCommitmentStatus(commitment: Commitment, options: AssetSyncOptions): Promise<Commitment> {
+    const provider = await this.context.providers.getProvider(commitment.chainId);
+    if (provider) {
+      return this.syncCommitmentStatusFromProvider(provider, commitment, options);
+    }
+    return commitment;
+  }
+
+  private async syncCommitmentStatusFromProvider(
+    provider: ethers.providers.Provider,
+    commitment: Commitment,
+    options: AssetSyncOptions,
+  ): Promise<Commitment> {
+    if (commitment.status === CommitmentStatus.QUEUED) {
+      const contract = this.context.contractConnector.connect<CommitmentPool>(
+        'CommitmentPool',
+        commitment.contractAddress,
+        provider,
+      );
+      const includedCount = toBN(
+        (
+          await promiseWithTimeout(
+            contract.getCommitmentIncludedCount(),
+            options?.providerTimeoutMs || DEFAULT_PROVIDER_QUERY_TIMEOUT_MS,
+          )
+        ).toNumber(),
+      );
+      if (
+        commitment.leafIndex &&
+        toBN(commitment.leafIndex).lt(includedCount) &&
+        commitment.creationTransactionHash
+      ) {
+        const includedEvent: CommitmentIncludedEvent = {
+          eventType: EventType.COMMITMENT_INCLUDED,
+          contractAddress: commitment.contractAddress,
+          commitmentHash: commitment.commitmentHash,
+          transactionHash: commitment.creationTransactionHash,
+          leafIndex: undefined,
+          encryptedNote: undefined,
+          rollupFee: undefined,
+          queuedTransactionHash: undefined,
+        };
+        const updatedCommitments = await this.context.executors.getEventExecutor().import([includedEvent], {
+          chainId: commitment.chainId,
+          walletPassword: options.walletPassword,
+        });
+        return updatedCommitments[0];
+      }
+    } else if (commitment.status === CommitmentStatus.INCLUDED) {
+      return this.checkCommitmentSpent(commitment, options?.providerTimeoutMs);
+    }
+    return commitment;
   }
 }
